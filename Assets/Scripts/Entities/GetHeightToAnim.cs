aed11e9eff49e3b020e35cda9d748e5b2209b787
StarkTheGnr <lordsomy@hotmail.com>
2020-04-22 11:57:27 +0200

    Fixed up the animations for the scientist

---
+++ Assets/Scripts/Entities/GetHeightToAnim.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetHeightToAnim : MonoBehaviour
+{
+    [SerializeField]
+    LayerMask layer;
+
+    [SerializeField]
+    Transform raycastOrigin;
+
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Ray ray = new Ray(raycastOrigin.position, -transform.up);
+        Debug.DrawRay(ray.origin, ray.direction);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, 10f, layer))
+        {
+            anim.SetFloat("Height", hit.distance);
+        }
+    }
+}
